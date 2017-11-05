c50802f20dacc28dd522eb3b22b8212bc629c5ad
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-05 21:13:05 +0000

    refactor: Created card object

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-$('.card-front').hide();
+const card = {
+	revealed: false,
+	picture: 1,
+	flipCard: function() {
+		$(this).toggle();
+		$(this).siblings().toggle();
+		if (this.revealed === false) {
+			this.revealed = true;
+		} else {
+			this.revealed = false;
+		}
+	}
+}
 
-$('.card-front, .card-back').click(function() {
-	$(this).toggle();
-	$(this).siblings().toggle();
-});
+$('.card-front, .card-back').click(card.flipCard);
