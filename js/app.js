f396994dfa2d09969644f5fb8ca0463f948b6680
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-06 22:18:04 +0000

    refactor: Complete overhaul of code
    Moved all cards into an array of 'card' objects so they are less complex and also added a function to insert each card's html into the page.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,43 @@
-// Global variable for currently revealed cards
+// Global variable for all cards
+const cards = [
+{revealed: false, html: "<h2>A</h2>"},
+{revealed: false, html: "<h2>A</h2>"},
+{revealed: false, html: "<h2>B</h2>"},
+{revealed: false, html: "<h2>B</h2>"},
+{revealed: false, html: "<h2>C</h2>"},
+{revealed: false, html: "<h2>C</h2>"},
+{revealed: false, html: "<h2>D</h2>"},
+{revealed: false, html: "<h2>D</h2>"},
+{revealed: false, html: "<h2>E</h2>"},
+{revealed: false, html: "<h2>E</h2>"},
+{revealed: false, html: "<h2>F</h2>"},
+{revealed: false, html: "<h2>F</h2>"},
+{revealed: false, html: "<h2>G</h2>"},
+{revealed: false, html: "<h2>G</h2>"},
+{revealed: false, html: "<h2>H</h2>"},
+{revealed: false, html: "<h2>H</h2>"},
+];
+
+// Global variable for revealed cards
 const revealedCards = [];
 
-const card = {
-	revealed: false,
-	revealCard: function(position, html) {
-		if (card.revealed === false) {
-			position.toggle();
-			position.siblings().toggle();
-			revealedCards.push(html);
-			card.revealed = true;
-		}
-	},
-	hideCard: function() {
-		if (this.revealed === true) {
-			$(this).toggle();
-			$(this).siblings().toggle();
-			this.revealed = false;
-		}
+// Assign cards to the Game Area
+const assignCards = function() {
+	let card = $('.card').first();
+	for (let i = 0; i < cards.length; i++) {
+		card.children(':first-child').append(cards[i].html);
+		card = card.next();
 	}
 }
 
-const card1 = Object.create(card);
-card1.html = "<h2>A</h2>";
-card1.position = $('#card-one');
+const revealCard = function(card) {
+	$(card).toggle();
+	$(card).siblings().toggle();
+}
 
-$('.card').children().click(function() {
-	card1.revealCard(card1.position, card1.html);
+// Main
+assignCards();
+
+$('.card').children(':last-child').click(function() {
+	revealCard(event.target);
 });
