f304580d500a7ad4e0707cdc8063b717c58014b0
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-06 19:43:17 +0000

    fix: Card can now only be revealed on click

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
+// Global variable for currently revealed cards
+const revealedCards = [];
+
 const card = {
 	revealed: false,
-	picture: 1,
-	flipCard: function() {
-		$(this).toggle();
-		$(this).siblings().toggle();
-		if (this.revealed === false) {
-			this.revealed = true;
-		} else {
+	revealCard: function(position, html) {
+		if (card.revealed === false) {
+			position.toggle();
+			position.siblings().toggle();
+			revealedCards.push(html);
+			card.revealed = true;
+		}
+	},
+	hideCard: function() {
+		if (this.revealed === true) {
+			$(this).toggle();
+			$(this).siblings().toggle();
 			this.revealed = false;
 		}
 	}
 }
 
-$('.card-front, .card-back').click(card.flipCard);
+const card1 = Object.create(card);
+card1.html = "<h2>A</h2>";
+card1.position = $('#card-one');
+
+$('.card').children().click(function() {
+	card1.revealCard(card1.position, card1.html);
+});
