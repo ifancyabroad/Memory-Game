16c76dd8475f4e8601fa3d8559c8f9f55e30fa86
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-06 22:55:42 +0000

    feat: Added function to push revealed cards to an array

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // Global variable for all cards
 const cards = [
-{revealed: false, html: "<h2>A</h2>"},
-{revealed: false, html: "<h2>A</h2>"},
-{revealed: false, html: "<h2>B</h2>"},
-{revealed: false, html: "<h2>B</h2>"},
+{revealed: false, html: "<h2>A</h2>", id: "card-one"},
+{revealed: false, html: "<h2>A</h2>", id: "card-two"},
+{revealed: false, html: "<h2>B</h2>", id: "card-three"},
+{revealed: false, html: "<h2>B</h2>", id: "card-four"},
 {revealed: false, html: "<h2>C</h2>"},
 {revealed: false, html: "<h2>C</h2>"},
 {revealed: false, html: "<h2>D</h2>"},
@@ -30,9 +30,25 @@ const assignCards = function() {
 	}
 }
 
+// Flip the card over on the Game Area
 const revealCard = function(card) {
 	$(card).toggle();
 	$(card).siblings().toggle();
+}
+
+// Add revealed card to the revealed cards array
+const addCard = function(card) {
+	for (let i = 0; i < cards.length; i++) {
+		if ($(card).parent().attr('id') == cards[i].id) {
+			cards[i].revealed = true;
+			revealedCards.push(cards[i].html)
+		}
+	}
+}
+
+// Check the revealed cards for matches
+const checkCards = function() {
+	
 }
 
 // Main
@@ -40,4 +56,5 @@ assignCards();
 
 $('.card').children(':last-child').click(function() {
 	revealCard(event.target);
+	addCard(event.target);
 });
